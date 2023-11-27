8b78d6c5c6a0cf4a63b6ee72c7db9c1a5dbf720a
omalcolmson <101271472+omalcolmson@users.noreply.github.com>
2023-11-27 12:31:12 -0500

    making a date function

--- data-extraction/extract.py
+++ data-extraction/extract.py
@@ -3,6 +3,20 @@ from datetime import date
 
 nlp = spacy.load("en_core_web_trf") #loads the pretrained model
 
+def createDate(dateString="") -> list:
+    '''
+    Takes in a string and returns a date as a list formatted as [month, day, year]
+    '''
+    dateList = []
+    if dateString == "":
+        today = date.today()
+        dateList[0] = int(today.strftime('%d'))
+        dateList[1] = int(today.strftime('%m'))
+        dateList[2] = int(today.strftime('%Y'))
+    else: #TODO: need to handle when dateString is not empty and has different formats
+        pass
+    return dateList
+
 def extractApptData(text: str, model: spacy.Language) -> dict:
     '''
     Extracts appointment data from text and organizes it into a dictionary with the following appointment data:
@@ -29,4 +43,4 @@ def extractWeatherData(text: str, model: spacy.Language) -> list:
             entities.append(ent.text)
     
     #TODO: need to validate the locations are 1) real, 2), if the location is a state or city, 
-    
+
