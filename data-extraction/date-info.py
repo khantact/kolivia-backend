4aa54b9de77bcb1a34e8a6cb325c5d7cc44ce887
omalcolmson <101271472+omalcolmson@users.noreply.github.com>
2023-11-27 21:24:42 -0500

    working on date extraction

---
+++ data-extraction/date-info.py
@@ -0,0 +1,8 @@
+months = {}
+daysOfTheWeek = {}
+
+monthsAsStrs = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER", "JAN", "FEB", "MAR", "APR", "JUN", "JUL", "AUG", "SEPT", "OCT", "NOV", "DEC"]
+
+daysOfTheWeekAsStrs = ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY", "MON", "TUES", "WED", "THURS", "FRI", "SAT", "SUN"]
+
+
--- data-extraction/extract.py
+++ data-extraction/extract.py
@@ -3,6 +3,27 @@ from datetime import date
 
 nlp = spacy.load("en_core_web_trf") #loads the pretrained model
 
+def identifyDate(text:str, model: spacy.Language) -> dict:
+    '''
+    Takes a string and extracts all date information from the string. This function assumes that the given string contains a date with at least a day of the week or a day of a month. If no other date information is given, the function will fill out the rest of the dictionary values with the current date information.
+    '''
+    doc = model(text)
+    dateInfo = []
+    
+    #extracting only the relevant date related info from the text
+    for ent in doc.ents:
+        if ent.label_ == "DATE":
+            for token in doc:
+                dateStr = "" #for storing dates formatted as words because they get separated as tokens
+                shapeStr = "" #for storing the shape of the token for dates formatted as words
+                if token.text == ent.text: #only works for dates formatted as digits
+                    dateInfo.append([token.text, token.shape_])
+                elif token.text in ent.text: #for when dates are formatted as words like day and month
+                    dateStr += token.text + " "
+                    shapeStr += token.shape_ + " "
+                if token.text not in ent.text and dateStr != "":
+                    dateInfo.append([dateStr.strip(), shapeStr.strip()])
+    
 def createDate(dateString="") -> list:
     '''
     Takes in a string and returns a date as a list formatted as [month, day, year]
@@ -16,7 +37,7 @@ def createDate(dateString="") -> list:
         dateList[2] = int(today.strftime('%Y'))
     else: #TODO: need to handle when dateString is not empty and has different formats
         dateStringAsList = dateString.split() #make the dateString a list
-        # check if spacy can identify dates formatted as MM/DD/YYYY 
+        # check if spacy can identify dates formatted as MM/DD/YYYY （it does)
     return dateList
 
 def extractApptData(text: str, model: spacy.Language) -> dict:
