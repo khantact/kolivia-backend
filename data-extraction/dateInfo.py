00bc9131491f5808d1fa2621a77eac13d45d9d45
omalcolmson <101271472+omalcolmson@users.noreply.github.com>
2023-11-29 21:12:58 -0500

    made new doc with better name

---
+++ data-extraction/dateInfo.py
@@ -0,0 +1,21 @@
+months = {}
+daysOfTheWeek = {}
+
+monthsofYear = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"]
+
+monthsAsStrs = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER", "JAN", "FEB", "MAR", "APR", "JUN", "JUL", "AUG", "SEPT", "OCT", "NOV", "DEC"]
+
+daysOfTheWeekAsStrs = ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY", "MON", "TUES", "WED", "THURS", "FRI", "SAT", "SUN"]
+
+months[1] = ["JANUARY", "JAN"]
+months[2] = ["FEBRUARY", "FEB"]
+months[3] = ["MARCH", "MAR"]
+months[4] = ["APRIL", "APR"]
+months[5] = ["MAY"]
+months[6] = ["JUNE", "JUN"]
+months[7] = ["JULY", "JUL"]
+months[8] = ["AUGUST", "AUG"]
+months[9] = ["SEPTEMBER", "SEPT", "SEP"]
+months[10] = ["OCTOBER", "OCT"]
+months[11] = ["NOVEMBER", "NOV"]
+months[12] = ["DECEMBER", "DEC"]
